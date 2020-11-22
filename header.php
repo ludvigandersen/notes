5a4cad5b05eb74f5b3cca90f62fa9c6db38b47ba
ludvig <ludvigrandersen@gmail.com>
2020-11-22 15:47:58 +0100

    adds session check to header.php

--- header.php
+++ header.php
@@ -1,3 +1,21 @@
 <header>
   <img src="img/music-notes-logo.jpg" alt="" id="logo">
+  <?php 
+    if(!isset($_SESSION)) { 
+      session_start(); 
+    }
+
+    if (isset($_SESSION['email'])){
+  ?>
+  <a href="artist.php">Artists</a>
+  <a href="album.php">Albums</a>
+  <a href="track.php">Tracks</a>
+
+  <form action="index.php" id="signOutForm" method="POST">
+    <input type="submit" name="signOut" value="Sign out">
+  </form>
+  <?php } else { ?>
+    <a href="index.php">Sign In</a>
+    <a href="index.php">Sign Up</a>
+  <?php } ?>
 </header>
