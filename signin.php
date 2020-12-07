ed1617a2599cb5bc9b62ccbce017744588ff52e1
ludvig <ludvigrandersen@gmail.com>
2020-12-07 12:30:33 +0100

    creating sessions on sign in

--- signin.php
+++ signin.php
@@ -1,24 +1,53 @@
 <?php
   session_start();
 
+  if(!isset($_SESSION['csrf'])){
+    $_SESSION['csrf'] = bin2hex(random_bytes(32));
+  }
+  
+
   if(isset($_SESSION['admin'])){
     header('Location: artist.php');
   }
   
-  if(isset($_POST['email'])){
-    require_once("src/user.php");
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+  if(isset($_POST['token'])){
+    if($_POST['token'] != $_SESSION['csrf']){
+      # Render some error
+      return;
+    } else {
+      if(isset($_POST['email'])){
+        require_once("src/user.php");
 
-    $user = new User();
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-    $results = $user->sign_in($email, $password);
-    $verified = $results[0];
+        $user = new User();
 
-    if($verified){
-      $_SESSION['admin'] = $results[1];
+        $results = $user->sign_in($email, $password);
+        $verified = $results[0];
 
-      header('Location: artist.php');
+        if($verified){
+          $csrf_token = bin2hex(random_bytes(32));
+
+          $_SESSION['admin'] = $results[1];
+          $_SESSION['csrf'] = $csrf_token;
+
+          $_SESSION['userId'] = $user->userId;
+          $_SESSION['email'] = $email;
+          $_SESSION['firstName'] = $user->firstName;
+          $_SESSION['lastName'] = $user->lastName;
+          $_SESSION['company'] = $user->company;
+          $_SESSION['address'] = $user->address;
+          $_SESSION['city'] = $user->city;
+          $_SESSION['state'] = $user->state;
+          $_SESSION['country'] = $user->country;
+          $_SESSION['postalCode'] = $user->postalCode;
+          $_SESSION['phone'] = $user->phone;
+          $_SESSION['fax'] = $user->fax;
+
+          header('Location: artist.php');
+        }
+      }
     }
   }
 ?>
@@ -28,6 +57,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Security-Policy" content="script-src http://notes.com/js/; img-src http://notes.com/; style-src http://notes.com/css/;">
   <link rel="stylesheet" href="css/style.css">
   <script src="js/jquery-3.5.1.js"></script>
   <script src="js/script.js"></script>
@@ -44,6 +74,7 @@
       <div>
         <input type="password" placeholder="Password" name="password" id="signInPassword" required>
       </div>
+      <input type="hidden" name="token" value=<?= $_SESSION['csrf']?>>
       <div>
         <input type="submit" value="Sign In" id="signInBtn">
       </div>
