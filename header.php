ce773bda85d2fe7a77d2366706f4c38d78bc6dee
ludvig <ludvigrandersen@gmail.com>
2020-12-07 12:29:09 +0100

    changes to header.php

--- header.php
+++ header.php
@@ -1,22 +1,34 @@
 <header>
-  <img src="img/music-notes-logo.jpg" alt="" id="logo">
+  <!-- <img src="img/music-notes-logo.jpg" alt="" id="logo"> -->
   <?php 
 
     if(!isset($_SESSION)) { 
       session_start(); 
     }
-    print_r($_SESSION);
+
     if (isset($_SESSION['admin'])){
   ?>
-  <a href="artist.php">Artists</a>
-  <a href="album.php">Albums</a>
-  <a href="track.php">Tracks</a>
-
-  <form action="index.php" id="signOutForm" method="POST">
-    <input type="submit" name="signOut" value="Sign out">
-  </form>
+  <ul>
+    <li><a href="artist.php">Artists</a></li>
+    <li><a href="album.php">Albums</a></li>
+    <li><a href="track.php">Tracks</a></li>
+    <?php if($_SESSION['admin'] != "true"){ ?>
+    <li><a href="cart.php">Cart</a></li>
+    <?php } ?>
+    <li class="rightHeader"><a href="user_update.php">Settings</a></li>
+    
+    <form action="index.php" id="signOutForm" method="POST">
+      <li>
+      <input type="submit" name="signOut" value="Sign out" id="signOut">
+      </li>
+    </form>
+  </ul>
+  
   <?php } else { ?>
-    <a href="signin.php">Sign In</a>
-    <a href="signup.php">Sign Up</a>
+  <ul>
+    <li><a href="signin.php">Sign In</a></li>
+    <li><a href="signup.php">Sign Up</a></li>
+  </ul>
   <?php } ?>
+  
 </header>
