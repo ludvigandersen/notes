109382544ceacc0d882c42e0607044d6b64877b2
ludvig <ludvigrandersen@gmail.com>
2020-12-07 12:31:29 +0100

    changes to user class

--- src/user.php
+++ src/user.php
@@ -2,39 +2,19 @@
   require_once('db_conn.php');  
   
   class User {
-    
-    # Inset user into database
-    function create($data){
-      global $pdo;
 
-      if ($data['password'] != $data['passwordRepeat']){
-        echo json_encode("passwords are not matching");
-        return;
-      }
-
-$query = <<<'SQL'
-      INSERT INTO customer
-      (FirstName, LastName, Password, Company, Address, City, State, Country, PostalCode, Phone, Fax, Email)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-SQL;
-
-      $pass_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-      $stmt = $pdo->prepare($query);
-      $insert_data = array(
-        $data['firstName'], $data['lastName'], $pass_hash,
-        $data['company'], $data['address'], $data['city'],
-        $data['state'], $data['country'], $data['postalCode'],
-        $data['phoneNumber'], $data['fax'], $data['email']
-      );
-      $result = $stmt->execute($insert_data);
-
-      # Check if query was successful
-      if($result){
-        echo true;
-      } else {
-        echo false;
-      }
-    }
+    public $userId;
+    public $email;
+    public $firstName;
+    public $lastName;
+    public $company;
+    public $address;
+    public $city;
+    public $state;
+    public $country;
+    public $postalCode;
+    public $phone;
+    public $fax;
 
     # Validate user based on $email and $password
     function sign_in($email, $password){
@@ -42,7 +22,7 @@ SQL;
       session_start();
 
 $query = <<<'SQL'
-      SELECT password, email
+      SELECT password, email, firstName, lastName, company, address, city, state, country, PostalCode, phone, fax, customerId
       FROM customer
       WHERE email=?
 SQL;
@@ -67,6 +47,20 @@ SQL;
         $stmt->execute(array($pass_hash));
         $admin = $stmt->fetch();
 
+
+        $this->userId = $user['customerId'];
+        $this->email = $email;
+        $this->firstName = $user['firstName'];
+        $this->lastName = $user['lastName'];
+        $this->company = $user['company'];
+        $this->address = $user['address'];
+        $this->city = $user['city'];
+        $this->state = $user['state'];
+        $this->country = $user['country'];
+        $this->postalCode = $user['PostalCode'];
+        $this->phone = $user['phone'];
+        $this->fax = $user['fax'];
+        
         // $_SESSION['email'] = $_POST['email'];
         if(password_verify($password, $admin[0])) {
           $admin = "true";
